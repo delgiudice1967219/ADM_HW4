f4fb28768aa323b358ccb7ca49613fde90e00574
Valeria Avino <avino.1905974@studenti.uniroma1.it>
2024-11-29 18:16:31 +0100

    fixes, this approach works

--- functions.py
+++ functions.py
@@ -1,21 +1,28 @@
 import math
 import numpy as np
+from scipy.sparse import csr_matrix
 
-def char_vec(seen_movies : set, all_movies_broadcast ):
+def char_vec(seen_movies, all_movies):
     '''
     Create a characteristic vector for a user's seen movies.
-
-    Parameters:
-    - seen_movies (set): A set of movie IDs that the user has seen.
-    - all_movies (set): A set of all possible movie IDs..
-
-    Returns:
-    - numpy.ndarray: A binary vector of length `len(all_movies)`, 
-      where 1 indicates that the user has seen the movie and 0 indicates they have not.
+    Return the result as a sparse matrix for memory efficiency.
     '''
-    all_movies = all_movies_broadcast.value
-    vector = [1 if movie in seen_movies else 0 for movie in all_movies]
-    return vector
+    all_movies = np.array(all_movies, dtype=int)
+
+    # Create a binary vector (sparse format)
+    vec = np.zeros(len(all_movies), dtype=int)
+
+    for movie in seen_movies:
+        # Locate the index of the movie in all_movies and set that position to 1
+        movie_idx = np.where(all_movies == movie)[0]
+        if movie_idx.size > 0:
+            vec[movie_idx[0]] = 1
+    
+    # Return as a sparse matrix (compressed sparse row format)
+    return csr_matrix(vec)
+
+
+import random
 
 
 
@@ -42,3 +49,33 @@ def create_signature(vec, hash_functions):
     return sign
 
 
+
+def minhash(vec, num_hash_functions, coeff_a, coeff_b, prime):
+    """
+    Generate a MinHash signature for a given characteristic vector using NumPy.
+    
+    Parameters:
+    - vec: A characteristic vector in sparse format (csr_matrix).
+    - num_hash_functions: The number of hash functions to use.
+    - coeff_a: Array of random coefficients 'a' for hash functions.
+    - coeff_b: Array of random coefficients 'b' for hash functions.
+    - prime: A prime number
+
+    Returns:
+    - sign: A NumPy array containing the MinHash signature, where each entry
+            is the minimum hash value of indices corresponding to 1 in `vec`.
+    """
+    # Find indices where vec == 1
+    nonzero_indices = vec.indices
+
+    # If no indices have value 1, return infinity for all hash functions
+    if len(nonzero_indices) == 0:
+        return np.full(num_hash_functions, np.inf)
+    
+    # Compute hash values for all non-zero indices using vectorized operations
+    hashes = (coeff_a[:, None] * nonzero_indices + coeff_b[:, None]) % prime    
+    
+    # Find the minimum hash value for each hash function
+    sign = np.min(hashes, axis=1)
+    
+    return sign
